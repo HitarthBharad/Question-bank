1beb3c736c6bf88d3c320e1223c9b25a234c10e7
HitarthBharad <38078378+HitarthBharad@users.noreply.github.com>
2020-04-21 15:30:47 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb+srv://hitarthBharad:iamHitarthBharad@question-0ppt8.mongodb.net/test?retryWrites=true&w=majority";
+const uri = "mongodb+srv://hitarthBharad:<password>@question-0ppt8.mongodb.net/test?retryWrites=true&w=majority";
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
 );
 const connection = mongoose.connection;
